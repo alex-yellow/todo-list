b963febfe245d308c409a4556c7fde75e86f61f8
alex-yellow <hitys2000@gmail.com>
2023-09-24 13:26:26 +0300

    added public

--- index.js
+++ index.js
@@ -1,35 +1,41 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const exphbs = require('express-handlebars');
+const express = require('express')
+const mongoose = require('mongoose')
+const path = require('path')
+const exphbs = require('express-handlebars')
 const todoRoutes = require('./controllers/todos')
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
-const app = express();
+const app = express()
 const hbs = exphbs.create({
-    defaultLayout: 'main',
-    extname: 'hbs'
-});
+  defaultLayout: 'main',
+  extname: 'hbs'
+})
 
 app.engine('hbs', hbs.engine)
-app.set('view engine', 'hbs');
-app.set('views', 'views');
+app.set('view engine', 'hbs')
+app.set('views', 'views')
 
-app.use(todoRoutes);
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')))
 
-async function start(){
-    try {
-        await mongoose.connect('mongodb://localhost:27017', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true 
-        });  
-        app.listen(PORT, function(){
-            console.log('Server has been started');
-        });  
-    } catch (error) {
-        console.log(error);
-    }
+app.use(todoRoutes)
+
+async function start() {
+  try {
+    await mongoose.connect(
+      'mongodb://localhost:27017/todos',
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true 
+      }
+    )
+    app.listen(PORT, () => {
+      console.log('Server has been started...')
+    })
+  } catch (e) {
+    console.log(e)
+  }
 }
 
-start();
-
+start()
